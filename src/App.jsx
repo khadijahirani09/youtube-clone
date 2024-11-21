41b03720335212d16f8c2d6bb95ea4779e2e803a
Khadija Hirani <khadijahirani09@gmail.com>
2024-11-21 17:16:14 +0530

    Fixed loading issue and updated Feed component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import {Route, Routes} from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import Video from './Pages/Video/Video'
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import Home from "./Pages/Home/Home";
+import Video from "./Pages/Video/Video";
 const App = () => {
   const[sidebar,setSidebar] = useState(true);
   return (
